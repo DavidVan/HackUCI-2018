e694d330e6b8001e1a9738d573123f013ca543c6
Jenny Luc <jeluc@ucsd.edu>
2018-02-03 01:30:33 -0800

    added the calculate_calories endpoint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
+const calculate_calories = require('./routes/calculate_calories');
 
 const index = require('./routes/index'); // Include our index.js file from 'routes' folder.
 
@@ -16,6 +17,6 @@ app.set('views', path.join(__dirname, 'views')); // Make sure we have the right 
 app.set('view engine', 'ejs'); // Set view engine to use ejs.
 
 app.use('/', index); // Serve the index page.
-
+app.use('/calculate_calories', calculate_calories);
 app.listen(1337);
 console.log('Server running on port 1337');
